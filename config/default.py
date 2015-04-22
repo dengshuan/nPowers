3ad4e0f3281c4fdc9d24cd6f36c03479e97a020e
dengshuan <dengshuan09@gmail.com>
2015-04-23 00:18:08 +0800

    fix upload path errors, untrack upload images

--- config/default.py
+++ config/default.py
@@ -1,5 +1,5 @@
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.curdir)
 UPLOAD_DIR = os.path.join(BASE_DIR, 'nPowers/static/uploads')
 CACHE_TYPE = 'simple'
 CSRF_ENABLED = True
